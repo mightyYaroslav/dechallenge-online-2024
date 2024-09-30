5a5f895cc5b3112b2ed66983b65ac27a8ef736f4
mightyYaroslav <yaroslav@nomadhomes.co>
2024-09-30 21:57:12 +0300

    remove extra files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CategoryService } from './category/category.service';
 import { HealthController } from './health/health.controller';
 import { CategoryController } from './category/category.controller';
@@ -8,7 +6,7 @@ import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, CategoryController, HealthController],
-  providers: [AppService, CategoryService, PrismaService],
+  controllers: [CategoryController, HealthController],
+  providers: [CategoryService, PrismaService],
 })
 export class AppModule {}
